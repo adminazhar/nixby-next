9ecbad1b8a7cdc3810ce82c44a98339c148a19b1
Azhar Khan <admin@azhark.com>
2022-09-08 19:20:44 +0530

    simple route

---
+++ src/layouts/Header/MainMenu.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link'
+
+export default function MainMenu() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/mobile">
+          <a>Mobile</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/blog/hello-world">
+          <a>Blog Post</a>
+        </Link>
+      </li>
+    </ul>
+  )
+}
