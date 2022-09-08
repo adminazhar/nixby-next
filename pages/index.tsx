b5fbbf9591d6ecb1afe323443d6e5a82729c3fc8
Azhar Khan <admin@azhark.com>
2022-09-08 19:21:13 +0530

    creatig homepage

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainMenu from '../src/layouts/Header/MainMenu.tsx';
+
+export default function Home() {
+  return (
+    <>
+      <MainMenu />
+      <div>
+        Hii Index
+      </div>
+    </>
+  )
+}
